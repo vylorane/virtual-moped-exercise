a85546c78ec3b0858164a54c60d07d28be1c3bd4
bloombar <amos@cs.nyu.edu>
2022-03-11 11:22:51 -0500

    adding message for main method

---
+++ test/edu/nyu/cs/TestDriveTest.java
@@ -0,0 +1,15 @@
+package edu.nyu.cs;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestDriveTest {
+    @Test
+    public void testMain() {
+        assertEquals(
+            "We expect the main method in the TestDrive class to behave as per the instructions",
+            "We have not created automated tests for the main method, so make sure you follow the instructions!"
+        );
+    }
+}
