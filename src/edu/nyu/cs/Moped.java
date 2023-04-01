10421e26b1f7e7d03d180f9e66a57b283d413495
vylorane <vy2063@gmail.com>
2023-03-31 23:39:15 -0400

    woooo

--- src/edu/nyu/cs/Moped.java
+++ src/edu/nyu/cs/Moped.java
@@ -2,6 +2,8 @@ package edu.nyu.cs;
 
 import java.util.Arrays;
 
+import javax.lang.model.util.ElementScanner6;
+
 /**
  * A virtual moped, roaming the streets of New York.
  * The signatures of a few methods are given and must be completed and used as indicated.
@@ -10,22 +12,63 @@ import java.util.Arrays;
  * The rest is up to you.
  */
 public class Moped {
+    private String facing;  // orientation
+    private int[] location; 
+    private int gasLevel;
 
+    public Moped(){
+        this.facing = "south";
+        this.location = new int[] {10,5};
+        this.gasLevel = 100;
+        // Moped moped1 = new Moped("north", "red");
+        
+    }
     /**
      * Sets the orientation of the moped to a particular cardinal direction.
      * @param orientation A string representing which cardinal direction at which to set the orientation of the moped.  E.g. "north", "south", "east", or "west".
      */
     public void setOrientation(String orientation) {
+        String mopedOri = orientation.toLowerCase();
+
+        if (mopedOri.equals("north")){
+            this.facing = "north";
+        }else if(mopedOri.equals("south")){
+            this.facing = "south";
+        }else if(mopedOri.equals("east")){
+            this.facing = "east";
+        }else if(mopedOri.equals("west")){
+            this.facing = "west";
+        }
+
+
 
     }
 
+    /**
+     * @param streetAndAvenue
+     * @return suffix
+     */
+    public String getSuffix(int streetAndAvenue){
+        String suffix = " ";
+        if (streetAndAvenue % 10 == 1 && streetAndAvenue != 11){
+            suffix = "st";
+        }else if (streetAndAvenue % 10 == 2 && streetAndAvenue != 12){
+            suffix = "nd";
+        }else if (streetAndAvenue % 10 == 3 && streetAndAvenue != 13){
+            suffix = "rd";
+        }else{
+            suffix = "th";
+        }
+
+        return suffix;
+    }
     /**
      * Returns the current orientation of the moped, as a lowercase String.
      * E.g. "north", "south", "east", or "west".
      * @return The current orientation of the moped, as a lowercase String.
      */
     public String getOrientation() {
-        return ""; // placeholder only... delete this!        
+        return this.facing; // placeholder only... delete this!    
     }
 
     /**
@@ -38,6 +81,20 @@ public class Moped {
      * Note that the suffixes for the numbers must be correct: i.e. the "st" in "1st", "nd" in "2nd", "rd" in "3rd", "th" in "4th", etc, must be correct.
      */
     public void printLocation() {
+        String suffixStreet = getSuffix(this.location[0]);
+        String suffixAvenue = getSuffix(this.location[1]);
+
+        String currentLocation = "Now at " + this.location[0] + suffixStreet + " St. and " + this.location[1] + suffixAvenue + " Ave, facing " + this.facing + ".";
+
+        if (this.location[0] == 79 && this.location[1] == 8 ){
+            System.out.println("American Museum of Natural History: Unlimited Tickets, Early Access, Store Discounts and Much More!");
+        }else if(this.location[0] == 74 && this.location[1] == 1){
+            System.out.println("Memorial Sloan Kettering: Leading The Fight Against Cancer. Always focused on you.");
+        }else if(this.location[0] == 56 && this.location[1] == 3){
+            System.out.println("Tina's Cuban Cuisine: 10% off your first online order! ");
+        }else if(this.location[0] == 12 && this.location[1] == 4){
+            System.out.println("The Strand: Where it all began: Bound by a Family");
+        }
 
     }
 
@@ -49,6 +106,24 @@ public class Moped {
      * This method must not print anything.
      */
     public void goLeft() {
+        if(this.gasLevel >= 5 && this.location[0] <= 200 && this.location[1]<= 10){
+            this.gasLevel-=5;
+            if (this.facing.equals("east")){
+                this.facing = "north";
+                this.location[0]++;
+            }else if(this.facing.equals("west")){
+                this.facing = "south";
+                this.location[0]--;
+            }else if (this.facing.equals("south")){
+                this.facing = "east";
+                this.location[1]--;
+            }else if(this.facing.equals("north")){
+                this.facing = "west";
+                this.location[1]++;
+            }
+        }else{
+            System.out.println("Oops! Not enough gas is left. Refill.");
+        } 
 
     }
 
@@ -60,6 +135,24 @@ public class Moped {
      * This method must not print anything.
      */
     public void goRight() {
+        if(this.gasLevel >= 5 && this.location[0] <= 200 && this.location[1]<= 10){
+            this.gasLevel-=5;
+            if (this.facing.equals("east")){
+                this.facing = "south";
+                this.location[0]--;
+            }else if(this.facing.equals("west")){
+                this.facing = "north";
+                this.location[0]++;
+            }else if (this.facing.equals("south")){
+                this.facing = "west";
+                this.location[1]++;
+            }else if(this.facing.equals("north")){
+                this.facing = "east";
+                this.location[1]--;
+            }
+        }else{
+            System.out.println("Oops! Not enough gas is left. Refill.");
+        } 
 
     }
 
@@ -70,6 +163,20 @@ public class Moped {
      * This method must not print anything.
      */
     public void goStraight() {
+        if(this.gasLevel >= 5 && this.location[0] <= 200 && this.location[1]<= 10){
+            this.gasLevel-=5;
+            if (this.facing.equals("east")){
+                this.location[1]--;
+            }else if(this.facing.equals("west")){
+                this.location[1]++;
+            }else if (this.facing.equals("south")){
+                this.location[0]--;
+            }else if(this.facing.equals("north")){
+                this.location[0]++;
+            }
+        }else{
+            System.out.println("Oops! Not enough gas is left. Refill.");
+        } 
 
     }
 
@@ -80,6 +187,20 @@ public class Moped {
      * This method must not print anything.
      */
     public void goBackwards() {
+        if(this.gasLevel >= 5 && this.location[0] <= 200 && this.location[1] <= 10){
+            this.gasLevel-=5;
+            if (this.facing.equals("east")){
+                this.location[1]++;
+            }else if(this.facing.equals("west")){
+                this.location[1]--;
+            }else if (this.facing.equals("south")){
+                this.location[0]++;
+            }else if(this.facing.equals("north")){
+                this.location[0]--;
+            }
+        }else{
+            System.out.println("Oops! Not enough gas is left. Refill.");
+        } 
 
     }
 
@@ -89,7 +210,7 @@ public class Moped {
      * @return The current gas level, as an integer from 0 to 100.
      */
     public int getGasLevel() {
-        return 0; // placeholder only... delete this!
+        return this.gasLevel; // placeholder only... delete this!
     }
 
     /**
@@ -100,6 +221,15 @@ public class Moped {
      *      We have run out of gas.  Bye bye!
      */
     public void printGasLevel() {
+        int gasLevel = this.getGasLevel();
+        int percentGasLevel = (gasLevel / 100) * 100;
+
+        if (this.gasLevel >= 5){
+            System.out.println("The gas tank is " + percentGasLevel + "% full");
+        }else{
+            System.out.print("We have run out of gas. Bye bye!");
+        }
+
 
     }
 
@@ -109,6 +239,7 @@ public class Moped {
      * Fills the gas level to the maximum.
      */
     public void fillGas() {
+        this.gasLevel = 100;
 
     }
 
@@ -119,7 +250,7 @@ public class Moped {
      * (In case you were wondering, status code 1 represents quitting as a result of an error of some kind).
      */
     public void park() {
-
+        System.exit(0);
     }
 
     /**
@@ -128,8 +259,12 @@ public class Moped {
      * Consumes gas with each block, and doesn't move unless there is sufficient gas, as according to the instructions.
      */
     public void goToXianFamousFoods() {
+        int currentStreet = this.location[0];
+        int currentAve = this.location[1];
 
     }
+
+    
 
     /**
      * Generates a string, containing a list of all the user commands that the program understands.
@@ -144,6 +279,15 @@ public class Moped {
      * @param location an int array containing the new location at which to place the moped, in the order {street, avenue}.
      */
     public void setLocation(int[] location) {
+        //this.location[0] = location[0];
+        //this.location[1] = location[1];
+
+        int newStr = location[0];
+        int newAve = location[1];
+
+        this.location[0] = newStr;
+        this.location[1] = newAve;
+
 
     }
 
@@ -153,8 +297,10 @@ public class Moped {
      */
     public int[] getLocation() {
         // the following two lines are placeholder... delete them and return this moped's correct coordinates.
-        int[] location = {3, 4}; // an example array at 3rd st and 4th Ave.... placeholder only... delete this!
-        return location;
+        int[] location = {this.location[0], this.location[1]}; // an example array at 3rd st and 4th Ave.... placeholder only... delete this!
+        return this.location;
     }
 
+
 }
+
